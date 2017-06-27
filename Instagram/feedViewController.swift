bf2803fd2fbd44af5684837b8dd220f5947dc261
Elan Halpern <elanh@fb.com>
2017-06-27 15:44:56 -0700

    added a logout button

---
+++ Instagram/feedViewController.swift
@@ -0,0 +1,46 @@
+//
+//  feedViewController.swift
+//  Instagram
+//
+//  Created by Elan Halpern on 6/27/17.
+//  Copyright © 2017 Elan Halpern. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class feedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOutInBackground{ (error: Error?) in
+            if let error = error {
+               print(error.localizedDescription)
+            } else {
+            self.dismiss(animated: true, completion: nil)
+            print("Logout sucessful.")
+            }
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
