04cff18908ac851d70396f426e60fce43c70dd56
Elan Halpern <elanh@fb.com>
2017-06-27 14:22:26 -0700

    Added a README file, set up gitattributes and gitignore. Created the login view controller and linked it to online database

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Elan Halpern on 6/27/17.
+//  Copyright © 2017 Elan Halpern. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: userNameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) -> Void in
+            if user != nil {
+                print("Login Sucessful!")
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        newUser.username = userNameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (sucess: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                //TODO
+                //ADD INDIVIDUAL ALERTS FOR DIFFERENT ERROR CODES
+            } else {
+                print("User Registered Sucessfully")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
