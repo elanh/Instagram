1c7b6e0bd7c58a08d86301ee2cc3a08212ec662a
Elan Halpern <elanh@fb.com>
2017-06-28 14:24:16 -0700

    Created a custom class for a post cell to edit the image, captions, and username

--- Instagram/PostCell.swift
+++ Instagram/PostCell.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
+
 
 class PostCell: UITableViewCell {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var postImage: PFImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
