e0a65d98c8563c3ebe4942a85e80c82850227f72
Elan Halpern <elanh@fb.com>
2017-06-28 14:20:39 -0700

    Added the ParseUI podfile so that I can convert the PFFile to a UIImage

---
+++ Instagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Instagram
+//
+//  Created by Elan Halpern on 6/28/17.
+//  Copyright © 2017 Elan Halpern. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
