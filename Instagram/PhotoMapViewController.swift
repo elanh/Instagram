ae78f5be88a9fbc573557e1b20bd197079bfe07a
Elan Halpern <elanh@fb.com>
2017-06-29 11:59:42 -0700

    Changed the UI so that when the create tab bar is selected, the photo library immediatly launches instead of a screen with a library and camera button.

--- Instagram/PhotoMapViewController.swift
+++ Instagram/PhotoMapViewController.swift
@@ -11,33 +11,47 @@ import UIKit
 class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITabBarControllerDelegate {
     
     var chosenImage: UIImage!
+    var imagePickerController: UIImagePickerController = UIImagePickerController()
     
     @IBOutlet weak var imageToPost: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadImagePicker()
         
         tabBarController?.delegate = self
         imageToPost.image = chosenImage
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
     }
     
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         if tabBarController.selectedIndex == 1 {
-//            loadImagePicker()
+             loadImagePicker()
         }
     }
     
+    func loadImagePicker() {
+        imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageToPost.image = originalImage
+        dismiss(animated: true, completion: nil)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let captionViewController = segue.destination as! CaptionViewController
+        captionViewController.imageToPost = imageToPost.image
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let captionViewController = segue.destination as! CaptionViewController
-        captionViewController.imageToPost = imageToPost.image
-    }
     
 }
